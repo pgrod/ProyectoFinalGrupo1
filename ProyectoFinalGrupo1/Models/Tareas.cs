5d6bb06f5d66e31975910908d07d6969bf6792c8
pgrod <149545353+pgrod@users.noreply.github.com>
2024-12-12 11:36:11 -0600

    Creación de carpetas

---
+++ ProyectoFinalGrupo1/Models/Tareas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalGrupo1.Models
+{
+    public class Tareas
+    {
+        public int Id { get; set; }
+        public string Tarea { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+
+}
---
+++ ProyectoFinalGrupo1/ViewModel/TareasViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ProyectoFinalGrupo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalGrupo1.ViewModel
+{
+    public class TareasViewModel
+    {
+        private readonly List<Tareas> tareas = new List<Tareas>();
+
+        public void EditarTarea(Tareas tareas)
+        {
+            var tareaCreada = tareas.Find(t => t.Id == tareas.Id);
+            if (tareaCreada != null)
+            {
+                tareaCreada.Tarea = tareas.Tarea;
+                tareaCreada.Descripcion = tareas.Descripcion;
+                tareaCreada.Estado = tareas.Estado;
+            }
+        }
+
+        public void RemoveTarea(int Id)
+        {
+            var tareas = tareas.Find(tareas=> tareas.Id == Id);
+            if (tareas != null)
+            {
+                tareas.Remove(tareas);
+            }
+        }
+    }
+}
